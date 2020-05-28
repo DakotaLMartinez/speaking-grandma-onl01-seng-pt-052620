bdf05523864f68a0d707e964cc3e7cb676a57639
Dakota Martinez <dakotaleemusic@gmail.com>
2020-05-28 16:39:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,9 @@
 
 def speak_to_grandma(phrase)
   "I LOVE YOU TOO PUMPKIN!"
-  # if (if) the phrase is not (!) all uppercase (upcase) then they're not shouting and we should respond with "HUH?! SPEAK UP, SONNY!"
+  # if (if) the phrase (phrase) is not equal (!=) to the all uppercase version of the phrase (phrase.upcase) then they're not shouting and we should respond with "HUH?! SPEAK UP, SONNY!"
+  if phrase != phrase.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  end
   
 end
